cad75ca1d7acd30d6421f116c6b51fa29d4c83b4
nickie kane <kameo712@gmail.com>
2020-01-02 19:01:24 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,13 +10,14 @@ end
 
 def print_first_directors_movie_titles
   pp directors_database
+  binding.pry
   hash_1 = {}
   hash_1 = directors_database
   hash_1
   row_index = 0 
   while row_index < directors_database.length do
         puts hash_1[row_index]['Stephen Spielberg'][:title]
-        binding.pry
+        
         puts '\n'
   row_index += 1
   end
