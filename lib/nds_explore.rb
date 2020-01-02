359af3435dc2bf0b78c8bd12ec682cb1f01623e9
nickie kane <kameo712@gmail.com>
2020-01-02 18:47:36 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,8 +4,19 @@ require 'directors_database'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  nil
+  require 'pp'
+  pp nds
 end
 
-def print_first_directors_movie_titles
+def print_first_directors_movie_titles(nds)
+  require 'pp'
+  pp nds
+  hash_1 = nds
+  puts hash_1
+  row_index = 0 
+  while row_index < nds.length do
+        puts hash_1[0]
+  row_index += 1
+  end
+  #return
 end
